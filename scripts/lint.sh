c69fed1a0a2b5fe6da1e6a3a80a9d64f0f69a29d
mdryaan <alikhurshid842001@gmail.com>
2026-05-17 05:45:06 +0530

    fix: scripts/lint.sh Docker command doesn't run ct lint
    Signed-off-by: mdryaan <alikhurshid842001@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,9 +8,8 @@ SRCROOT="$(cd "$(dirname "$0")/.." && pwd)"
 echo -e "\n-- Linting all Helm Charts --\n"
 docker run \
      -v "$SRCROOT:/workdir" \
-     --entrypoint /bin/sh \
+     --workdir /workdir \
      quay.io/helmpack/chart-testing:v3.3.1 \
-     -c cd /workdir \
      ct lint \
      --config .github/configs/ct-lint.yaml \
      --lint-conf .github/configs/lintconf.yaml \
